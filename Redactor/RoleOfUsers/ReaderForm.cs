347c0ca64bca097dbb359efcd265be824f254b0d
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-12-30 17:13:43 +0200

    reader form

--- Redactor/RoleOfUsers/ReaderForm.cs
+++ Redactor/RoleOfUsers/ReaderForm.cs
@@ -5,18 +5,38 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Redactor.RoleOfUsers
 {
     public partial class ReaderForm : Form
     {
+        private static List<string> _names = new List<string>(); // имена авторов
+        private static List<string> _titles = new List<string>(); // названия статей
+        private static string _path = $"{Directory.GetCurrentDirectory()}\\AuthorArticles";
+
+
         public User ReaderUser { get; set; }
         public ReaderForm(User reader)
         {
             InitializeComponent();
             ReaderUser = reader;
+            CreateAuthorsList();
+        }
+
+
+        // Заполнение именами авторов списка
+        public void CreateAuthorsList()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(_path);
+
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+
+            foreach(var item in directories)
+            {
+                authorsLB.Items.Add(item.Name);
+            }
         }
     }
 }
